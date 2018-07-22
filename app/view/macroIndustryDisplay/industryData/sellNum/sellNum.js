a6f3da4e2ab5893fb6e110741c913f4cf1c79953
yangxiuyuan <894011908@qq.com>
2018-07-22 22:25:57 +0800

    数据展示初始化以及echart的demo

---
+++ app/view/macroIndustryDisplay/industryData/sellNum/sellNum.js
@@ -0,0 +1,11 @@
+angular.module('myApp.macroIndustryDisplay.sellNum', [
+
+])
+
+    .config(function($stateProvider, $urlRouterProvider){
+
+    })
+
+    .controller('sellNumCtrl',function($scope, $route, $http) {
+
+    });
--- app/view/macroIndustryDisplay/macroIndustryDisplay.js
+++ app/view/macroIndustryDisplay/macroIndustryDisplay.js
@@ -1,5 +1,6 @@
-angular.module('myApp.macroIndustryDisplay', [
-    'myApp.macroIndustryDisplay.generalInfo'
+var app=angular.module('myApp.macroIndustryDisplay', [
+    'myApp.macroIndustryDisplay.generalInfo',
+    'myApp.macroIndustryDisplay.sellNum'
 ])
 
     .config(function($stateProvider,$urlRouterProvider){
@@ -9,7 +10,50 @@ angular.module('myApp.macroIndustryDisplay', [
                 templateUrl: 'view/macroIndustryDisplay/generalInfo/generalInfo.html',
                 controller: 'GeneralInfoCtrl'
             })
+        $stateProvider
+            .state('macroIndustryDisplay.sellNum',{
+                url:'/sellNum',
+                templateUrl: 'view/macroIndustryDisplay/industryData/sellNum/sellNum.html',
+                controller: 'sellNumCtrl'
+            })
     })
 
     .controller('MacroIndustryDisplayCtrl',function($scope, $route, $http) {
+        $scope.toshowDataPane=false;
+        $scope.showDataPane=function () {
+            $scope.toshowDataPane=true;
+        }
+        $scope.hideDataPane=function () {
+            $scope.toshowDataPane=false;
+        }
+
+        var myChart = echarts.init(document.getElementById('main'));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 360, 10, 10, 20]
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
     });
+
+
+
+
+
