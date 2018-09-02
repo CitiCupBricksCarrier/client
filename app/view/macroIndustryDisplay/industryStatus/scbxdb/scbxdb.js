42d2dd06b8918a27a4022f026de9d861e8883168
yangxiuyuan <894011908@qq.com>
2018-09-02 12:09:10 +0800

    行业地位(部分

---
+++ app/view/macroIndustryDisplay/industryStatus/scbxdb/scbxdb.js
@@ -0,0 +1,13 @@
+angular.module('myApp.macroIndustryDisplay.scbxdb', [
+
+])
+
+    .config(function($stateProvider, $urlRouterProvider){
+
+    })
+
+    .controller('scbxdbCtrl',function($scope, $route, $http) {
+
+
+
+    });
