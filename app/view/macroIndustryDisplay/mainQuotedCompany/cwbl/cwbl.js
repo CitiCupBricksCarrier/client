bcf1edca564f2f21fd025ffde5844147dd56a663
yangxiuyuan <894011908@qq.com>
2018-09-12 00:06:17 +0800

    下拉框构造

--- app/view/macroIndustryDisplay/mainQuotedCompany/cwbl/cwbl.js
+++ app/view/macroIndustryDisplay/mainQuotedCompany/cwbl/cwbl.js
@@ -7,6 +7,18 @@ angular.module('myApp.macroIndustryDisplay.cwbl', [
     })
 
     .controller('cwblCtrl',function($scope, $route, $http) {
+        $scope.toShow=false;
+        $scope.str="";
+        $scope.check=function (value){
 
+           if(document.getElementById(value).checked==true){
+               $scope.str+=value;
+                document.getElementById('input').innerHTML=$scope.str+'<img src="view/macroIndustryDisplay/mainQuotedCompany/pic/arrow.png" style="width: 6px;height: 5px;float: right;margin-top: 7px;margin-right: 5px">';
+           }
+           else{
+                $scope.str.replace(value,"");
+               document.getElementById('input').innerHTML=$scope.str+'<img src="view/macroIndustryDisplay/mainQuotedCompany/pic/arrow.png" style="width: 6px;height: 5px;float: right;margin-top: 7px;margin-right: 5px">';
 
+           }
+        };
     });
