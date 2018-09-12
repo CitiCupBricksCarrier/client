ebda0c2060dc3b4fe6d3e5d3a6dfee3e2ced82ff
yangxiuyuan <894011908@qq.com>
2018-09-12 12:36:41 +0800

    财务比率比较初始化2

--- app/view/macroIndustryDisplay/mainQuotedCompany/cwbl/cwbl.js
+++ app/view/macroIndustryDisplay/mainQuotedCompany/cwbl/cwbl.js
@@ -8,15 +8,48 @@ angular.module('myApp.macroIndustryDisplay.cwbl', [
 
     .controller('cwblCtrl',function($scope, $route, $http) {
         $scope.toShow=false;
+        $scope.toShowPane=true;
         $scope.str="";
+        $scope.firstStr=""
+        $scope.isFirst=false;
         $scope.check=function (value){
 
            if(document.getElementById(value).checked==true){
-               $scope.str+=value;
+               if($scope.str==""){
+                   $scope.firstStr=value;
+                   $scope.isFirst=true;
+                   $scope.str+=value;
+               }
+               else{
+                   $scope.isFirst=false;
+                   $scope.str+=','+value;
+                   if($scope.str.split(',').length>=3){
+                       document.getElementById('input').style.height='42px';
+                   }
+               }
                 document.getElementById('input').innerHTML=$scope.str+'<img src="view/macroIndustryDisplay/mainQuotedCompany/pic/arrow.png" style="width: 6px;height: 5px;float: right;margin-top: 7px;margin-right: 5px">';
            }
            else{
-                $scope.str.replace(value,"");
+               if($scope.isFirst){
+                       $scope.str=$scope.str.replace(value,"");
+               }
+               else{
+                   if(value==$scope.firstStr){
+                       $scope.str=$scope.str.replace(value+',',"");
+                        $scope.firstStr=$scope.str.split(',')[0];
+                        if($scope.str.indexOf(',')==-1) $scope.isFirst=true;
+                       if($scope.str.split(',').length<=2) document.getElementById('input').style.height='21px';
+
+                   }
+                   else {
+                       $scope.str=$scope.str.replace(','+value,"");
+                       if($scope.str.indexOf(',')==-1){ $scope.isFirst=true;
+                       }
+                       if($scope.str.split(',').length<=2) document.getElementById('input').style.height='21px';
+
+                   }
+               }
+
                document.getElementById('input').innerHTML=$scope.str+'<img src="view/macroIndustryDisplay/mainQuotedCompany/pic/arrow.png" style="width: 6px;height: 5px;float: right;margin-top: 7px;margin-right: 5px">';
 
            }
