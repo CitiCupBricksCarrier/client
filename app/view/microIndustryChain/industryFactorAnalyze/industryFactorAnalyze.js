654213f74cd4e282452606c7aa46267099ceec1b
161250181 <498690270@qq.com>
2018-09-16 12:17:58 +0800

    。。

--- app/view/microIndustryChain/industryFactorAnalyze/industryFactorAnalyze.js
+++ app/view/microIndustryChain/industryFactorAnalyze/industryFactorAnalyze.js
@@ -9,7 +9,14 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
 
         //指标部分
         $scope.index_recommend = ['xxx1', 'xxx2', 'xxx3'];
-        $scope.index_selectable = ['xxx1','xxx2','xxx3'];
+        $scope.index_selectable = ['安全性-存货周转率','安全性类-速动比率','安全性类-总资产周转率', '成长-ROE变动', '成长类-GPOA变动', '成长类-毛利润增长率',
+            '分析师类-一致预期PB', '分析师类-一致预期预测营业收入', '分析师类-最近一个月券商覆盖数量（券商家数）变化', '估值类-股息率', '估值类-市销率的倒数',
+            '估值类-市盈率的倒数', '价量-6日成交额标准差', '价量-6日成交额均值（千元）', '价量-20日特雷诺比率', '价量-60日特雷诺比率', '盈利质量-累计成本费用利润率',
+            '盈利质量-所得税占盈利总额百分比'];
+        var index_selectable_eng = ['inventory_turnover', 'quick_ratio','turnover_of_total_assets','ROE_change','GPOA_change','profit_growth_rate',
+            'consistence_expectation','consistence_expectation_predict_profit','dealers_num_change_1M','dividend_rate','market_selling_rate_rec',
+            'P_E_ratio_rec', 'turnover_std_6D', 'turnover_mean_6D', 'Tenore_ratio_20D', 'Tenore_ratio_60D', 'cost_profit_margin_cumu', 'incometax_profit_percent'];
+
         $scope.index_unselectable = ['zzz1','zzz2', 'zzz3'];
 
         $scope.description_toShow = '点击查看指标详细信息';
@@ -18,6 +25,10 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
 
         $scope.index_selected = '';
         var index_selected_list = [];
+        $scope.index_selected_list = [];
+
+        var index_default = '安全性-存货周转率';
+
 
         //方法部分
         $scope.method_list = [
@@ -29,11 +40,13 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
             {name: '复合因子IC', description: '当您想要研究复合因子的选股能力、稳定性时的最佳选择'}
         ];
         $scope.method_selected = '';
+        // console.log($scope.method_list)
 
-        console.log($scope.method_list)
+        var method_default = 'IC-IR';
+
         /**
-         * ----------------------------------------
-         * ----------------------------------------
+         * -----------------------------------------------------------------------------
+         * -----------------------------------------------------------------------------
          * 监听
          */
         //点击标题
@@ -51,6 +64,19 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
         $scope.clickToShowDescription_method = function (item){
 
         }
+
+        //进行分析按钮的监听
+        $scope.clickToAnalyze = function () {
+            if(index_selected_list.length == 1){        //普通方法
+                analyze_single(index_selected_list[0], $scope.method_selected);
+            }
+        }
+
+        //复合因子确认分析按钮的监听
+        $scope.clickSureToAnalyze = function(){
+
+        }
+
         /**
          * 页面加载完毕时
          */
@@ -67,7 +93,7 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
 
                 var item = $(this).parent().children('a').text();
                 // console.log(item)
-                var liList = $('.part .section .list_container li');
+                var liList = $('.part .index_container .section .list_container li');
                 // console.log(liList)
                 for(var i = 0; i < liList.length; i++){         //推荐指标和全部指标里同一个指标要同步
                     var curLi = liList[i];
@@ -89,12 +115,13 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
                     // console.log(checkBox_child.prop('checked'))
                     if(checkBox_child.prop('checked')){
                         if(index_selected_list.indexOf(a_child.text()) < 0){
-                            index_selected_list.push(a_child.text());
+                            index_selected_list.push(a_child.text().replace(/^\s+|\s+$/g,""));
                         }
                     }
                 }
                 // console.log(index_selected_list)
                 $scope.index_selected = index_selected_list.toString();
+                $scope.index_selected_list = index_selected_list;
 
                 //如果选择多个，就只能选择复合因子,并自动选中
                 if(index_selected_list.length > 1){
@@ -104,8 +131,12 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
                     $('.part .method_container .section .list_container .checkBox:last').prop('checked', true);
                     $scope.method_selected = '复合因子IC';
                 }
-                else{
+                else{       //如果只有一个，不能选择复合因子
                     $('.part .method_container .section .list_container .checkBox').attr('disabled', false);
+                    $('.part .method_container .section .list_container .checkBox:last').prop('checked', false);
+                    $('.part .method_container .section .list_container .checkBox:last').attr('disabled', true);
+                    $($('.part .method_container .section .list_container .checkBox')[1]).prop('checked', true);
+                    $scope.method_selected = 'IC-IR';
                 }
 
                 //分析按钮的可用设置
@@ -127,23 +158,62 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
                 $('.part .method_container .section .list_container .checkBox:checked').prop('checked', false);
                 $(this).prop('checked', true);
 
-                $scope.method_selected = $(this).parent().text();
-
+                $scope.method_selected = $(this).parent().text().replace(/^\s+|\s+$/g,"");
+                // console.log($scope.method_selected)
                 $scope.$apply();        //应用更改
             })
 
+            setDefaultIndexAndMethod();
         })
 
+
         /**
-         * ----------------------------------------
-         * ----------------------------------------
+         * 设置默认指标和方法
+         */
+        function setDefaultIndexAndMethod() {
+            $('.part .index_container .section .list_container li').each(function () {
+                // console.log($(this).children('a').text().replace(/^\s+|\s+$/g,""))
+                if($(this).children('a').text().replace(/^\s+|\s+$/g,"") == index_default){
+                    $(this).children('.checkBox').prop('checked', true);
+                    index_selected_list = [index_default];
+                    $scope.index_selected_list = [index_default];
+                    $scope.index_selected = index_selected_list.toString();
+                }
+            })
+            $($('.part .method_container .section .list_container .checkBox')[1]).prop('checked', true);
+            $scope.method_selected = method_default;
+
+            $scope.$apply();
+        }
+
+        /**
+         * ----------------------------------------------------------------
+         * ----------------------------------------------------------------
          * 属性处理
          */
-        var indexList = ['xxx1', 'xxx2', 'xxx3'];
+        var indexList = ['安全性-存货周转率','安全性类-速动比率','安全性类-总资产周转率', '成长-ROE变动', '成长类-GPOA变动', '成长类-毛利润增长率',
+            '分析师类-一致预期PB', '分析师类-一致预期预测营业收入', '分析师类-最近一个月券商覆盖数量（券商家数）变化', '估值类-股息率', '估值类-市销率的倒数',
+            '估值类-市盈率的倒数', '价量-6日成交额标准差', '价量-6日成交额均值（千元）', '价量-20日特雷诺比率', '价量-60日特雷诺比率', '盈利质量-累计成本费用利润率',
+            '盈利质量-所得税占盈利总额百分比'];
         var descriptionList = [
-            'xxx1 description',
-            'xxx2 description',
-            'xxx3 description'
+            '对流动资产周转率的补充说明,是衡量企业销售能力及存货管理水平的综合性指标',
+            '企业速动资产与流动负债的比率，速动资产是企业的流动资产减去存货和预付费用后的余额，主要包括现金、短期投资、应收票据、应收账款等项目',
+            '企业一定时期的销售收入净额与平均资产总额之比,它是衡量资产投资规模与销售水平之间配比情况的指标',
+            '股东股权投资的报酬率的变化率',
+            'GPOA的变化率',
+            '利润总额的增长率',
+            '基于各券商分析师的调查(研究报告,电话,Email等等)的上市公司盈利预期数据平均值',
+            '基于各券商分析师的调查(研究报告,电话,Email等等)的上市公司营业收入数据平均值',
+            '最近一个月券商覆盖数量（券商家数）变化',
+            '一年的总派息额与当时市价的比例',
+            '总市值除以主营业务收入或者股价除以每股销售额的倒数',
+            '每股市價除以每股盈利的倒数',
+            '成交额在6天内的波动情况',
+            '6天成交额的均值',
+            '每单位风险获得的风险溢价，是投资者判断某一基金管理者在管理基金过程中所冒风险是否有利于投资者的判断指标（以20天计）',
+            '每单位风险获得的风险溢价，是投资者判断某一基金管理者在管理基金过程中所冒风险是否有利于投资者的判断指标（以60天计）',
+            '企业一定期间累计的利润总额与成本、费用总额的比率',
+            '企业的所得税占盈利总额百分比'
         ];
         var numList = [1,2,3];
 
@@ -160,5 +230,39 @@ angular.module('myApp.microIndustryChain.industryFactorAnalyze', [
                 result.num = numList[tempI];
             }
             return result;
+        }
+
+
+        /**
+         * ----------------------------------
+         * ----------------------------------
+         * http请求
+         */
+        //获取最近选择人数
+        $http({
+            method: 'post',
+            url: 'http://localhost:8080/CorrelationAnalysis/IndexClicks'
+        }).then(function successCallBack(response) {
+            console.log(response.data);
+
+        },function errorCallBack(response) {
+            console.error('获取最近选择人数失败');
+        });
+
+        //进行分析，单个指标的
+        function analyze_single(index, method) {
+            $http({
+                method: 'post',
+                url: 'http://localhost:8080/CorrelationAnalysis/AnalysisIndustry',
+                params: {
+                    index: index,
+                    analysisMethod: method
+                }
+            }).then(function successCallBack(response) {
+                console.log(response.data);
+
+            },function errorCallBack(response) {
+                console.error('分析失败');
+            });
         }
     });
