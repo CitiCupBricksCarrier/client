6b2d9141b4b8f29de3c0ca6f8368a51797bb0d47
zzw <1332901986@qq.com>
2018-10-28 15:13:05 +0800

    注册和积分充值 对接后

--- app/view/personal/personalMain.js
+++ app/view/personal/personalMain.js
@@ -7,6 +7,43 @@ angular.module('myApp.personalMain', [
     })
 
     .controller('PersonalMainCtrl',function($scope, $route, $http, $state) {
+        var userid = "";
+
+        $http({
+            url: urlHead + 'getSession',
+            method: 'post',
+            // contentType: "application/json",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            withCredentials: true
+        }).then(function successCallBack(response) {
+            // console.log(response.data)
+            var data = response.data;
+            userid = data;
+            // console.log(111, response, data);
+        }, function errorCallBack(response) {
+            console.log("erreor");
+        });
+
+        $scope.charge = function(){
+            var credits = parseInt($('#creditsChoice input[name="credit"]:checked ').val());
+            // console.log(credits)
+            $http({
+                method: 'post',
+                url: urlHead + 'creditsCharge',
+                // headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+                //cache: true, //避免多次请求后台数据
+                params: {
+                    id:userid,
+                    credits:credits
+                }
+            }).then(function (response) {
+                console.log(11,response);
+            }, function () {
+                console.error("charge Failed");
+            });
+            // document.getElementsByClassName("fade")[].style.display = "none";
+
+        };
 
         $(document).ready(function(){
 
