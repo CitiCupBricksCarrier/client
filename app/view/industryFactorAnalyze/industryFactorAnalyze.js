c418fcadc8edb9cf48ae5ed3c5e99059b9339c38
161250181 <498690270@qq.com>
2018-11-03 00:18:19 +0800

    修改部分js引用为min.js，宏观部分更换图片路径

--- app/view/industryFactorAnalyze/industryFactorAnalyze.js
+++ app/view/industryFactorAnalyze/industryFactorAnalyze.js
@@ -6,6 +6,24 @@ angular.module('myApp.industryFactorAnalyze', [
     })
 
     .controller('IndustryFactorAnalyzeCtrl',function($scope, $route, $http, $state) {
+        $http({
+            method: 'post',
+            url: urlHead + 'getSession',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            withCredentials: true
+            //cache: true, //避免多次请求后台数据
+        }).then(function (response) {
+            if (response.data != "null") {
+                $scope.haslogined = true;
+            }
+            else {
+                $scope.haslogined = false;
+                $state.go('login');
+            }
+        }, function () {
+            console.error("session error");
+        });
+
         $scope.toShowResult=false;
         $scope.RESULT='正在分析...';
         //指标部分
